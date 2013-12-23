0eedca554ab44788b9d461e08270f7fbab4d19fb
Paul Colby <git@colby.id.au>
2013-12-23 14:48:00 +1100

    Stubbed out the ConsoleListener interface.

--- src/qmf1/ConsoleListener.hpp
+++ src/qmf1/ConsoleListener.hpp
@@ -1,2 +1,29 @@
 /// Copyright Paul Colby 2013.
 /// @todo Add (Apache?) license.
+
+#include <qpid/console/ConsoleListener.h>
+
+class MyConsoleListener : public qpid::console::ConsoleListener {
+
+public:
+    virtual void brokerConnected(const qpid::console::Broker &broker);
+
+    virtual void brokerDisconnected(const qpid::console::Broker &broker);
+
+    virtual void newPackage(const std::string &package);
+
+    virtual void newClass(const qpid::console::ClassKey &classKey);
+
+    virtual void newAgent(const qpid::console::Agent &agent);
+
+    virtual void delAgent (const qpid::console::Agent &agent);
+
+    virtual void objectProps(qpid::console::Broker &broker, qpid::console::Object &object);
+
+    virtual void objectStats(qpid::console::Broker &broker, qpid::console::Object &object);
+
+    virtual void event(qpid::console::Event &event);
+
+    virtual void brokerInfo(qpid::console::Broker &broker);
+
+};
