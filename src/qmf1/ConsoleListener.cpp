060c23106275d4a761e5092c9ff10fe8a82d35a1
Paul Colby <git@colby.id.au>
2013-12-23 15:01:49 +1100

    Removed an unnecessary #include.

--- src/qmf1/ConsoleListener.cpp
+++ src/qmf1/ConsoleListener.cpp
@@ -3,9 +3,8 @@
 
 #include "ConsoleListener.hpp"
 
-#include <pcp/pmapi.h> // Note, the order in which these are included matters
-#include <pcp/impl.h>  // more for older versions of PCP, so don't reorder them
-#include <pcp/pmda.h>  // without testing against older versions of PCP.
+#include <pcp/pmapi.h>
+#include <pcp/impl.h>
 
 void ConsoleListener::brokerConnected(const qpid::console::Broker &broker) {
     __pmNotifyErr(LOG_DEBUG, "broker (%s) %s connected",
