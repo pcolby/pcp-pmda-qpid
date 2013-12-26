8c2dfaec879cc71c8fc36a0d85bd44bf01a2f2f3
Paul Colby <git@colby.id.au>
2013-12-26 16:53:34 +1100

    Fleshed out the other Qpid connection options.

--- src/qmf1/QpidPmda.cpp
+++ src/qmf1/QpidPmda.cpp
@@ -26,13 +26,26 @@ protected:
     virtual boost::program_options::options_description supported_options() const
     {
         using namespace boost::program_options;
-        options_description options("Options");
-        options.add_options()
+        options_description connectionOptions("Broker connection options");
+        connectionOptions.add_options()
             ("broker,b", value<string_vector>()->
              default_value(string_vector(1, "ampq://localhost:5672"), "ampq://localhost:5672")
-             PCP_CPP_BOOST_PO_VALUE_NAME("url"), "message broker url(s)");
-        options.add(pcp::pmda::supported_options());
-        return options;
+             PCP_CPP_BOOST_PO_VALUE_NAME("url"), "message broker url(s)")
+            ("heartbeat", value<double>()
+             PCP_CPP_BOOST_PO_VALUE_NAME("interval"), "heartbeat interval in seconds")
+            ("protocol", value<std::string>(), "version of AMQP to use (e.g. amqp0-10 or amqp1.0)")
+            ("tcp-nodelay", value<bool>()PCP_CPP_BOOST_PO_IMPLICIT_VALUE(true),
+             "whether nagle should be enabled")
+            ("transport", value<std::string>(), "underlying transport to use (e.g. tcp, ssl, rdma)");
+        options_description authenticationOptions("Broker authentication options");
+        authenticationOptions.add_options()
+            ("username", value<std::string>(), "username to authenticate as")
+            ("password", value<std::string>(), "password, if needed by SASL mechanism")
+            ("sasl-mechanisms", value<std::string>(), "acceptable SASL mechanisms")
+            ("sasl-min-ssf", value<unsigned>(), "minimum acceptable security strength factor")
+            ("sasl-max-ssf", value<unsigned>(), "maximum acceptable security strength factor")
+            ("sasl-service", value<std::string>(), "service name, if needed by SASL mechanism");
+        return connectionOptions.add(authenticationOptions).add(pcp::pmda::supported_options());
     }
 
     virtual pcp::metrics_description get_supported_metrics()
