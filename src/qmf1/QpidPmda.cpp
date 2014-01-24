3fb897efec9cac5b66e7a1d0922e113164b13b47
Paul Colby <git@colby.id.au>
2014-01-24 20:11:08 +1100

    Updated copyright notices for recently updates files.

--- src/qmf1/QpidPmda.cpp
+++ src/qmf1/QpidPmda.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Paul Colby
+ * Copyright 2013-2014 Paul Colby
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
